f2600e1cfb7e6bf784da85576bf828c6ce6be24f
Emma HB <emmanuelle.bolognini@gmail.com>
2022-01-25 21:32:25 +0100

    Iteration 1

---
+++ src/components/home/BeersList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BeersList extends React.Component {
+  render () {
+    return (
+      <>
+        
+      </>
+    ) 
+  }
+}
+
+export default BeersList;
---
+++ src/components/home/NewBeer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NewBeer extends React.Component {
+  render () {
+    return (
+      <>
+      </>
+    ) 
+  }
+}
+
+export default NewBeer;
