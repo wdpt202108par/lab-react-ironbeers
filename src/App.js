6ac8e137380a99a465d912a0d2a0cdc665125f54
evanina <eva.degano@gmail.com>
2022-01-25 21:59:52 +0100

    iteration 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from "react-router";
+import axios from "axios";
 import Home from "./components/Home";
+import BeerList from "./components/BeerList";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+class App extends Component {
+  state = {
+    beers: []
+  }
+
+  getAllBeers = () => {
+    axios.get("https://ih-beers-api2.herokuapp.com/beers")
+      .then(responseFromApi => {
+        console.log("responseFromApi 👇", responseFromApi.data);
+
+        this.setState({
+          beers: responseFromApi.data
+        });
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount() {
+    this.getAllBeers();
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/beers" render={(routerProps) => <BeerList {...routerProps} beers={this.state.beers} />} />
+        <Route path="/beers/:beerId" />
+      </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
