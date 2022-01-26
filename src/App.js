9e100d66e099bd647a7886729e71dcc6460e7369
frenchToast <juliekogan1@gmail.com>
2022-01-26 16:45:29 +0100

    iteration 4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './homepage';
 import { Switch, Route } from 'react-router-dom';
 import Allbeers from './components/Allbeers';
 import Navbar from './components/Navbar';
+import BeerDetails from './components/Beerdetails';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Route exact path="/" component={Homepage} />
         <>
           <Navbar />
-          <Route path="/beers" component={Allbeers} />
+          <Route exact path="/beers" component={Allbeers} />
+          <Route path="/beers/:_id" component={BeerDetails} />
           {/* <Route path="/random-beer" component={Randombeer} /> */}
           {/* <Route path="/new-beer" component={Newbeer} /> */}
         </>
---
+++ src/components/Beerdetails.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+import reactRouterDom from 'react-router-dom';
+
+class Beerdetails extends React.Component {
+  state = {};
+  componentDidMount() {
+    this.getDetails();
+  }
+  getDetails = () => {
+    axios
+      .get(
+        `https://ih-beers-api2.herokuapp.com/beers/${this.props.match.params._id}`
+      )
+      .then((response) => {
+        this.setState(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  render() {
+    return (
+      <>
+        <img src={this.state.image_url} alt="beerpick"></img>
+        <h1>{this.state.name}</h1>
+        <h2>{this.state.tagline} </h2>
+        <p>{this.state.first_brewed}</p>
+        <p>{this.state.attenuation_level}</p>
+        <p>{this.state.description}</p>
+        <p>{this.state.contributed_by}</p>
+      </>
+    );
+  }
+}
+
+export default Beerdetails;
