474280bb07fefbae18a547e0ab2867039c2dde8a
Emma HB <emmanuelle.bolognini@gmail.com>
2022-01-27 19:36:04 +0100

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
-import Home from './components/home/Home'
+
+import { Switch, Route } from 'react-router-dom'; // HERE
+
+import BeersList from './components/BeersList';
+import RandomBeer from './components/RandomBeer';
+import NewBeer from './components/NewBeer';
+
+import Home from './components/Home'
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div className="App">
-      <Home />
-    </div>
+    <Header />
+
+    <Switch> 
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/beers" component={BeersList}/>
+      <Route exact path="/random-beer" component={RandomBeer} />
+      <Route exact path="/new-beer" component={NewBeer} />
+    </Switch>
+
+  </div>
   );
 }
 
--- src/components/BeersList.js
+++ src/components/BeersList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class BeersList extends React.Component {
   render () {
     return (
       <>
-        
+        <h1><Link to="/beers">All Beers</Link></h1>
       </>
     ) 
   }
