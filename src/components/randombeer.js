83e3534b610e76cf25ee2ceb0ac78585c298c621
kthomas <kevin.allan.thomas@gmail.com>
2022-01-27 21:16:44 +0100

    Last commits

--- src/components/randombeer.js
+++ src/components/randombeer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class Random extends Component{
-
-
+  
 render(){
       return(
           <div>
               <h1>Random Beer</h1>
+
           </div>
         )
     }
