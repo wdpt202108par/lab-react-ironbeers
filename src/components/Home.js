8ba89cf86f7a8f4b5ae0e68e45c502dc5e68c8b7
Emma HB <emmanuelle.bolognini@gmail.com>
2022-01-28 11:51:13 +0100

    Iteration 5

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ class Home extends React.Component {
       </div>
       <div className="random-beer-container" >
         <img src={randomBeerImage} alt="random beer"/>
-        <h1>Random Beer</h1>
+        <h1><Link to="/random-beer">Random Beer</Link></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mi ipsum, varius a felis vitae, rutrum egestas augue. Fusce sem dolor, aliquet non est sit amet, hendrerit ornare dolor. Donec imperdiet aliquam quam a tincidunt. Etiam id accumsan nibh, vel placerat augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam magna magna, dictum ac lectus at, molestie bibendum quam. Morbi ac ante urna.</p>          
       </div>
       <div className="new-beer-container" >
--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 
 class RandomBeer extends React.Component {
+  state = {
+    randomBeer:''
+  }
+  
+  getRandomBeer = () => {
+    axios.get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+      .then(response => {
+        console.log('randomBeer',response.data)
+        this.setState({randomBeer: response.data})
+      })
+      .catch(err => console.log("error", err))
+  }
+
+  componentDidMount() {
+    this.getRandomBeer();
+  }
+
   render () {
     return (
-      <>
-      </>
+      <div className="single-beer-container">
+        <img src={this.state.randomBeer.image_url} alt=""></img>
+        <div className="cols">
+          <div className="col-left">
+            <h1>{this.state.randomBeer.name}</h1>
+            <h3>{this.state.randomBeer.tagline}</h3>
+          </div>
+          <div className="col-right">
+            <h3>{this.state.randomBeer.attenuation_level}</h3>
+            <h5>{this.state.randomBeer.first_brewed}</h5>
+          </div>
+        </div>
+        <p>{this.state.randomBeer.description}</p>
+        <h6>{this.state.randomBeer.contributed_by}</h6>
+      </div>
     ) 
   }
 }
